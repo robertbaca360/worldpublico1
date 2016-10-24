8a0fcb3b3e05dcbc2501b5d1a32831e4f3bc60eb
Gabri Doga <bestsuperflycode@gmail.com>
2016-10-24 13:12:55 -0400

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   config.app_domain = 'worldpublic.herokuapp.com'
 
-  Email
+  # Email
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: config.app_domain }
