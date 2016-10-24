bc8ef1dbb56e491ca08a9575cfe44a93292a5a68
Gabri Doga <bestsuperflycode@gmail.com>
2016-10-24 14:00:05 -0400

    make it better

--- db/migrate/20161010120445_add_name_to_users.rb
+++ db/migrate/20161010120445_add_name_to_users.rb
@@ -2,7 +2,7 @@ class AddNameToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name, :string
     add_column :users, :address, :string
-    add_column :users, :tel, :integer
+    add_column :users, :tel, :string
     add_column :users, :website, :string
     add_column :users, :category, :string
     add_column :users, :about, :string
